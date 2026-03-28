9b0b77f59131c05112171cb4a7d4061941ee79c8
elsa <elsamathew.canfig@gmail.com>
2026-03-28 12:22:29 +0530

    inventory and billing partial working

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Bill(models.Model):
+    created_by = models.ForeignKey(User , on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"Bill {self.id} - {self.total}"
+
+class BillItem(models.Model):
+    bill = models.ForeignKey(Bill , on_delete = models.CASCADE , related_name = 'items')
+    product = models.ForeignKey(Product , on_delete = models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"Bill {self.product.name} - {self.quantity}"
